3556d4f5a5d898067489e51522601cbcc6500b14
ALUMNO-A <ALUMNO-A@UCSP.AULAB>
2017-03-28 10:43:52 -0500

    clase3/

---
+++ clase3.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{  /* int c=5;
+    int x=c++;
+
+    cout << x << endl;
+    cout << c << endl;
+
+
+###################################################
+
+  int c=5;
+    int x=++c;
+
+    cout << x << endl;
+    cout << c << endl;
+    return 0;
+###################################################*/
+   /* int a,b,c,d;
+    a=2;
+    a+=3;
+    b=7;
+    b-=2;
+    c=2;
+    c*=4;
+    d=8;
+    d/=2;
+    cout<<a<<b<<c<<d<<endl;
+    return 0;
+######################################################*/
+/*int a,b,z;
+cout<<"ingrese a"<<endl;
+cin>>a;
+cout<<"ungrese b"<<endl;
+cin>>b;
+if (a>b){
+    z=a;
+
+}
+else{z=b;}
+cout<<z<<endl;
+return 0;*/
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+//int a,b,z;cout<<"ingrese a"<<endl;cin>>a;cout<<"ungrese b"<<endl;cin>>b; z=(a>b)?a:b;cout<<z;
+int score[10];
+score[3] =10;
+cout<<score<<endl;
+}
