b53a42d7628261498f413c5b0cac18941272664e
jhuberucsp <jhuber.prado@ucsp.edu.pe>
2017-05-12 11:29:46 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include "Vector.h"
+#include "Point.h"
+using namespace std;
+int main(){
+    /*Point P1;
+    P1.x=1;
+    P1.y=2;
+    P1.print();
+*/
+ /*   Vector V;
+
+    V.Star.x=3;
+    V.Star.y=5;
+    V.End.x=3;
+    V.End.y=3;
+
+    V.print();*/
+
+}
